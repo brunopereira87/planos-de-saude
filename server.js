5b0f117e9bfe28cc9014eb013f968c3e3394346c
Bruno Pereira <bruno_emanuel87@hotmail.com>
2021-06-18 20:45:56 -0300

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const { resolve } = require('path');
 
 app.use('/',express.static(
   resolve(__dirname, './dist')
 ));
 
+app.use(cors);
 app.listen(process.env.PORT || 3000, err => {
   if(err) return console.log(err);
 
